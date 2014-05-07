913b04b098abd959b0761748991e8e05a7f303ad
guoj <dev.guoj@gmail.com>
2014-05-07 16:09:10 +0800

    remove repeated codes.

--- prisoner.py
+++ prisoner.py
@@ -24,25 +24,20 @@ class Dilemma:
 
     def compute(self, human_choice, computer_choice):
         """ If you play with the computer, and then computer_choice should be random_move(). """
+        self.original_move = self.previous_move
+        self.previous_move = computer_choice
+        self.rounds += 1
+        
         if (human_choice == computer_choice == 0):
-            self.original_move = self.previous_move
-            self.previous_move = computer_choice
             self.human_payoff += 3
             self.computer_payoff += 3
         elif (human_choice == computer_choice == 1):
-            self.original_move = self.previous_move
-            self.previous_move = computer_choice
             self.human_payoff += 1
             self.computer_payoff += 1
         elif (human_choice == 0 and computer_choice == 1):
-            self.original_move = self.previous_move
-            self.previous_move = computer_choice
             self.computer_payoff += 5
         elif (human_choice == 1 and computer_choice == 0):
-            self.original_move = self.previous_move
-            self.previous_move = computer_choice
             self.human_payoff += 5
-        self.rounds += 1
         Dilemma.display_message(self, computer_choice)
 
     def random_move(self):
