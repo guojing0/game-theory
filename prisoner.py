54ba73c989fcc6f0122a1099ac3836db4dc9117a
guoj <dev.guoj@gmail.com>
2014-05-07 15:13:01 +0800

    comments and docs

--- prisoner.py
+++ prisoner.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python
 import random
 
-class Dilemma: # Prisoner's Dilemma
+# The program is to simulate the prisoner's dilemma
+# Cooperation move is 0 and defection move is 1
+# tit_for_tat() and win_stay_lose_switch() are two different strategies for human_choice
+# Of course you can choose 0 or 1 yourself for human_choice
+# random_move() is for computer_choice
+
+class Dilemma:
 
     def __init__(self):
         self.rounds = 0
         self.human_payoff = 0
         self.computer_payoff = 0
-        self.original_move = 1
+        self.original_move = 1 # The move before previous_move
         self.previous_move = 0
 
     def display_message(self, choice):
-        print "Rounds: %d" % self.rounds
-        print "Human payoff: %d" % self.human_payoff
-        print "Computer payoff: %d" % self.computer_payoff
-        print "Computer choice: %d" % choice
+        print 'Rounds: %d' % self.rounds
+        print 'Human payoff: %d' % self.human_payoff
+        print 'Computer payoff: %d' % self.computer_payoff
+        print 'Computer choice: %d' % choice
 
-    def compute(self, human_choice, computer_choice): # computer's choice should be random_move()
+    def compute(self, human_choice, computer_choice):
+        """ If you play with the computer, and then computer_choice should be random_move(). """
         if (human_choice == computer_choice == 0):
             self.original_move = self.previous_move
             self.previous_move = computer_choice
@@ -45,7 +52,7 @@ class Dilemma: # Prisoner's Dilemma
             Dilemma.display_message(self, computer_choice)
 
     def random_move(self):
-        return random.randint(0, 1) # Cooperation is 0 and defection is 1
+        return random.randint(0, 1)
 
     def tit_for_tat(self):
         return self.previous_move
