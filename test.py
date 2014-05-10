915a109147d4bf7475a779e8e2ceb85d5da32f70
guoj <dev.guoj@gmail.com>
2014-05-10 20:11:20 +0800

    debugs

--- test.py
+++ test.py
@@ -9,12 +9,12 @@ if __name__ == '__main__':
 
     for i in range(5000):
         x.compute_score(x.win_stay_lose_switch(), x.random_move(), display_info=False)
-        y.compute_score(y.bayesian_move(2), y.random_move(), display_info=False)
+        y.compute_score(y.bayesian_move(2), x.random_move(), display_info=False)
 
         if (x.human_payoff > x.computer_payoff):
             num_x += 1
         elif (y.human_payoff > y.computer_payoff):
             num_y += 1
 
-    print 1.0 * num_x / 5000
-    print 1.0 * num_y / 5000
+    print 'X %.3f' % (1.0 * num_x / 5000)
+    print 'Y %.3f' % (1.0 * num_y / 5000)
