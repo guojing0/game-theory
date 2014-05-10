cbabc1ecd54f05f78a825019d423b92602584fac
guoj <dev.guoj@gmail.com>
2014-05-10 10:32:31 +0800

    Set initial history

--- prisoner.py
+++ prisoner.py
@@ -6,6 +6,7 @@ import random
 # tit_for_tat() and win_stay_lose_switch() are two different strategies for human_choice
 # Of course you can choose 0 or 1 yourself for human_choice
 # random_move() is for computer_choice
+# bayesian_move() is a strategy based on Bayes' theorem
 
 class Dilemma:
 
@@ -13,10 +14,10 @@ class Dilemma:
         self.rounds = 0
         self.human_payoff = 0
         self.computer_payoff = 0
-        self.original_move = 1 # The move before previous_move
+        self.original_move = 0 # The move before previous_move
         self.previous_move = 0
 
-        self.history = [] # The history of computer's moving
+        self.history = [0, 1] # The history of computer's moving. The initial list prevents bayesian_move() from ZeroDivisionError
         self.prob_c = 0.5
         self.prob_d = 0.5
         self.prob_c_over_d = 0.5
@@ -30,9 +31,9 @@ class Dilemma:
     def compute(self, human_choice, computer_choice):
         """ If you play with the computer, and then computer_choice should be random_move(). """
         self.rounds += 1
+        self.history.append(computer_choice)
         self.original_move = self.previous_move
         self.previous_move = computer_choice
-        self.history.append(computer_choice)
 
         if (human_choice == computer_choice == 0):
             self.human_payoff += 3
@@ -59,13 +60,13 @@ class Dilemma:
             self.original_move = 1
         return self.original_move
 
-    def bayesian_move(self):
+    def bayesian_move(self): # Needs fixes for simplicity
         if (self.history == []):
             return 0
         else:
             self.prob_c = 1.0 * self.history.count(0) / len(self.history)
             self.prob_d = 1.0 * self.history.count(1) / len(self.history)
-            self.prob_c_over_d = 0.5 * self.prob_c / self.prob_d # Bayesian prob. THE BUG IS HERE, PROB_D MAY BE 0
+            self.prob_c_over_d = 0.5 * self.prob_c / self.prob_d
 
             if (self.prob_c_over_d >= 0.5):
                 return 0
--- test.py
+++ test.py
@@ -2,10 +2,14 @@ if __name__ == '__main__':
     import prisoner
 
     x = prisoner.Dilemma()
+    y = prisoner.Dilemma()
+    x.history = [0, 1]
 
-    for i in range(10):
-        print x.prob_c
-        print x.prob_d
-        print x.prob_c_over_d
-        x.compute(x.bayesian_move(), x.random_move())
-    print x.history
+    hist = [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1]
+
+    for i in hist:
+        print x.prob_c, y.prob_c
+        print x.prob_d, y.prob_d
+        print x.prob_c_over_d, y.prob_c_over_d
+        x.compute(x.bayesian_move(), i)
+        y.compute(y.bayesian_move(), i)
