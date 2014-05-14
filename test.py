c84f6ae5000c0e05babb3daa3681b7dadc30d80c
guoj <dev.guoj@gmail.com>
2014-05-14 13:50:55 +0800

    test

--- test.py
+++ test.py
@@ -3,10 +3,5 @@ if __name__ == '__main__':
 
     x = prisoner.Dilemma()
 
-    for m in range(1,10):
-        for i in range(100):
-            x.compute_score(x.bayesian_move(m), x.random_move(), display_info=False)
-
-    print len(x.history)
-    print x.history.count(0)
-    print x.history.count(1) 
+    for i in range(50):
+        x.compute_score(x.bayesian_move(), x.random_move())
