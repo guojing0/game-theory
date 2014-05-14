ba3256122eb4bb312f00023601fb4ebefc059c0c
guoj <dev.guoj@gmail.com>
2014-05-14 18:43:00 +0800

    test

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     x = prisoner.Dilemma()
 
     for i in range(100):
-        x.compute_score(x.bayesian_move(2), x.random_move())
+        x.compute_score(x.bayesian_move(10), x.random_move())
