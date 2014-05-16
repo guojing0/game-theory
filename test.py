313d9f4351f62a2cfb7f96ef3c4a92f603d773bd
guoj <dev.guoj@gmail.com>
2014-05-16 15:51:22 +0800

    test

--- test.py
+++ test.py
@@ -1,7 +1,21 @@
-if __name__ == '__main__':
-    import prisoner
+import prisoner
 
-    x = prisoner.Dilemma()
+x = prisoner.Dilemma()
 
+def init_test():
     for i in range(100):
-        x.compute_score(x.bayesian_move(10), x.random_move())
+        x.compute_score(x.____(), x.random_move())
+
+def efficiency_test():
+    y = prisoner.Dilemma()
+    for i in range(100):
+        x.compute_score(x.____(), x.random_move())
+        y.compute_score(y.____(), x.random_move())
+
+def payoff_test():
+    for i in range(1000):
+        x.compute_score(x.____(), x.random_move())
+    print (1.0*x.human_payoff/(x.human_payoff+x.computer_payoff))
+
+if __name__ == '__main__':
+    pass
